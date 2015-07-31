aed6b3cb3034e1ce1a23005ebdad5c3b414db04c
Kai Rubarth <krubar@gmail.com>
2015-07-31 12:27:05 +0200

    make rewritten work with fullpath

--- lib/rack/url.rb
+++ lib/rack/url.rb
@@ -1,11 +1,8 @@
 require 'rack'
 
 module Rack
-
   module Rewritten
-
     class Url
-
       attr_accessor :base_url
 
       def initialize(app, &block)
@@ -17,37 +14,39 @@ module Rack
         instance_eval(&block) if block_given?
       end
 
-      def is_internal_target?(url)
-        url.nil? or url.start_with?('/') or url.start_with?(@base_url)
+      def internal_target?(url)
+        url.nil? || url.start_with?('/') || url.start_with?(@base_url)
       end
 
-      def is_external_target?(url)
-        !is_internal_target?(url)
+      def external_target?(url)
+        !internal_target?(url)
       end
 
       def call(env)
         req = Rack::Request.new(env)
 
-        subdomain = env["SUBDOMAIN"] ? "#{env["SUBDOMAIN"]}:" : ""
+        subdomain = env['SUBDOMAIN'] ? "#{env['SUBDOMAIN']}:" : ''
 
         path = "#{subdomain}#{req.path_info}"
         path.downcase! if downcase_before_lookup?
 
         target_url = ::Rewritten.translate(path)
 
-        if is_external_target?(target_url)
+        if external_target?(target_url)
           r = Rack::Response.new
           r.redirect(target_url, 301)
           r.finish
-        elsif ::Rewritten.includes?(path.chomp("/")) or backwards=( translate_backwards? && ::Rewritten.exist_translation_for?(path) )
+        elsif ::Rewritten.includes?(req.fullpath) || ::Rewritten.includes?(path.chomp('/')) || backwards = (translate_backwards? && ::Rewritten.exist_translation_for?(path))
 
-          to = ::Rewritten.includes?(path.chomp("/")) || path
+          to = ::Rewritten.includes?(req.fullpath)
+          to ||= ::Rewritten.includes?(path.chomp('/')) || path
 
           current_path = ::Rewritten.get_current_translation(to)
-          current_path = current_path.split(":").last
+          current_path = current_path.split(':').last
+          current_path_with_query = current_path
           current_path = current_path.split('?')[0]
 
-          if (current_path == req.path_info) or (::Rewritten.base_from(req.path_info) == current_path) or ::Rewritten.has_flag?(path, 'L')
+          if (req.fullpath == current_path_with_query || current_path == req.path_info) || (::Rewritten.base_from(req.path_info) == current_path) || ::Rewritten.flag?(path, 'L')
             # if this is the current path, rewrite path and parameters
             tpath, tparams = split_to_path_params(to)
             env['QUERY_STRING'] = Rack::Utils.build_nested_query(tparams.merge(req.params))
@@ -59,12 +58,12 @@ module Rack
 
             r = Rack::Response.new
 
-            new_path = env["rack.url_scheme"].dup
-            new_path << "://"
-            new_path << env["HTTP_HOST"].dup.sub(/^#{subdomain.chomp(':')}\./, '')
+            new_path = env['rack.url_scheme'].dup
+            new_path << '://'
+            new_path << env['HTTP_HOST'].dup.sub(/^#{subdomain.chomp(':')}\./, '')
             new_path << current_path
             new_path << ::Rewritten.appendix(path) unless backwards
-            new_path << '?' << env["QUERY_STRING"] unless (env["QUERY_STRING"]||'').empty?
+            new_path << '?' << env['QUERY_STRING'] unless (env['QUERY_STRING'] || '').empty?
 
             r.redirect(new_path, 301)
             a = r.finish
@@ -85,24 +84,20 @@ module Rack
         @translate_backwards
       end
 
-      def translate_backwards=(yes_or_no)
-        @translate_backwards = yes_or_no
-      end
+      attr_writer :translate_backwards
 
       def downcase_before_lookup?
         @downcase_before_lookup
       end
 
-      def downcase_before_lookup=(yes_or_no)
-        @downcase_before_lookup = yes_or_no
-      end
+      attr_writer :downcase_before_lookup
 
       def translate_partial?
         @translate_partial
       end
 
       def translate_partial=(yes_or_no)
-        $stderr.puts "DEPRECATED. Please use Rewritten.translate_partial"
+        $stderr.puts 'DEPRECATED. Please use Rewritten.translate_partial'
         ::Rewritten.translate_partial = yes_or_no
       end
     end
